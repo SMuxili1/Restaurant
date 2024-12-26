a5f1719ad8cade59a4f29854724d59690a1b6e9f
SMuxili1 <ramiromuxili.mwangobrain@outlook.pt>
2024-12-26 18:32:20 +0100

    refact: all projects

--- src/Restaurants/Restaurants.Web.API/Controllers/RestaurantController.cs
+++ src/Restaurants/Restaurants.Web.API/Controllers/RestaurantController.cs
@@ -1,24 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Restaurants.Application.Restaurants;
-using Restaurants.Application.Restaurants.Dtos;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Restaurants.Commands;
+using Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
+using Restaurants.Application.Restaurants.Queries.GetRestaurantById;
 
 namespace Restaurants.Web.API.Controllers
 {
     [Route("api/restaurants")]
     [ApiController]
-    public class RestaurantController(IRestaurantsService restaurantsService) : ControllerBase
+    public class RestaurantController(IMediator mediator) : ControllerBase
     {
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var restaurants = await restaurantsService.GetAllRestaurants();
+            var restaurants = await mediator.Send(new GetAllRestaurantsQuery());
             return Ok(restaurants);
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetById([FromRoute]int id) 
+        public async Task<IActionResult> GetById([FromRoute] int id)
         {
-            var restaurant = await restaurantsService.GetById(id);
+            var restaurant = await mediator.Send(new GetRestaurantByIdQuery(id));
+
             if (restaurant is null)
                 return NotFound();
 
@@ -26,9 +29,9 @@ namespace Restaurants.Web.API.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateRestaurant([FromBody]CreateRestaurantDto createRestaurantDto)
+        public async Task<IActionResult> CreateRestaurant([FromBody] CreateRestaurantCommand command)
         {
-            int id = await restaurantsService.Create(createRestaurantDto);
+            int id = await mediator.Send(command);
             return CreatedAtAction(nameof(GetById), new { id }, null);
         }
     }
